3b2b4525a30918a8fd4a92cecd33149f1884d7e7
Bmeza05 <brianmeza952@gmail.com>
2021-06-08 18:56:53 -0600

    initial commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,14 @@
+for x in range(0,151,1):
+    print(x)
+
+for y in range(5,1001,5):
+    print(y)
+
+for z in range(0,101,1):
+    print(z)
+    if z%5:
+        print("coding")
+
+for y in range(0,500001,1):
+    x=sum(y)
+    print(x)
